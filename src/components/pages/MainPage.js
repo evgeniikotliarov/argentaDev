5cc20fd0be6b5940a06f6084965e59f009adcf70
evgenii <evk@iopent.ru>
2020-08-12 16:47:12 +0300

    Добавил hook.

---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class MainPage extends React.Component {
+  render() {
+    return (
+      <div className="main-page" style={{width: '100px', height: '50px', color: 'red'}}>
+      </div>
+    );
+  }
+}
